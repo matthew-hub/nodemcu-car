b5da45b98d2d69cf744532756088fcfbefb4577d
matthew-hub <mateuszr430@gmail.com>
2021-10-03 12:38:26 +0200

    add wifi connection

--- node-car/src/main.cpp
+++ node-car/src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <Hash.h>
+
+const char* ssid = "Home";
+const char* pwd = "genesiswifi";
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
+    delay(10);
+
+    Serial.println("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, pwd);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    };
+    Serial.println("");
+    Serial.println("WiFi connected");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 }
