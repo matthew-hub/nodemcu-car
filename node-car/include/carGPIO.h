78bde0bd35139d42868a141624270749b0a22a98
matthew-hub <mateuszr430@gmail.com>
2021-10-06 14:37:56 +0200

    take all GPIO in one place

---
+++ node-car/include/carGPIO.h
@@ -0,0 +1,13 @@
+/* LIST OF USED GPIOs */
+
+// DC MOTOR
+#define MOTORIN0 9
+#define MOTORIN1 10
+
+// SERVO
+#define CARSERVO D2
+#define HCSERVO D1
+
+// HCSR04
+#define TRIGGER D3
+#define ECHO D4
